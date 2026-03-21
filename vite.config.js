67679261080c99e2e4dedc0eab48e30f39825218
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-03-21 19:14:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// basic vite i apply
+// normmal vite are in use 
 export default defineConfig({
   plugins: [react()],
   build: {
