fa340be2db35496cb4ffb8e8d7850bdcfd7c50c6
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-03-29 18:02:59 +0530

    Fix typo in vite.config.js comment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// normmal vite are in use 
+// normmal vite are in use ..
 export default defineConfig({
   plugins: [react()],
   build: {
